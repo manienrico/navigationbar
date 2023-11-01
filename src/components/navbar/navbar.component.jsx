56daf1599b13301b65745c830f6ac64090821792
Enrico Mani <manienrico13@gmail.com>
2023-11-01 16:41:33 +0300

    routes functional, still styling multi layer nav

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,17 +1,33 @@
 import { Fragment } from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
 
-import { ReactComponent } from 'react'
+import { ReactComponent as Logo } from '../../assets/logotype.svg'
 
 import './navbar.styles.css'
-import { NavLink, Outlet } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <Fragment>
         <header>
-            <NavLink className="" to="/">ZARA</NavLink>
-            <nav className='nav-container'>
-                <NavLink></NavLink>
+            <NavLink className="" to="/">
+              <Logo />
+            </NavLink>
+            <nav>
+                {/* <NavLink></NavLink> */}
+                <ul>
+                  <li><NavLink to="home" className="navlink level1">Home</NavLink></li>
+                  <li><NavLink className="navlink level1 collections">Collections</NavLink>
+                    <ul className="level2-container">
+                      <li><NavLink className="navlink level2">Summer Collections</NavLink></li>
+                      <li><NavLink className="navlink level2">Sale 40% Off</NavLink></li>
+                      <li><NavLink className="navlink level2">Ceramics</NavLink></li>
+                      <li><NavLink className="navlink level2">Melamina</NavLink></li>
+                    </ul>
+                  </li>
+                  <li><NavLink to="about" className="navlink level1">About</NavLink></li>
+                  <li><NavLink to="contact" className="navlink level1">Contact</NavLink></li>
+                  <li><NavLink to="shop" className="navlink level1">Shop</NavLink></li>
+                </ul>
             </nav>
         </header>
         <Outlet />
